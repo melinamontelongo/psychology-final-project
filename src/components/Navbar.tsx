6db866e2fc511b44fd7e0fc91d332c1f858b7599
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-09-03 16:13:38 -0300

    feat: responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,42 @@ import { NavbarDictionary } from "@/content/navbar";
 
 const Navbar = ({ lang }: { lang: string }) => {
     return (
-        <nav className="navbar absolute px-4 py-2 2xl:px-10 2xl:py-5 w-full backdrop-blur bg-zinc-800/70 flex justify-between">
-            <Link href="/" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.introduction}</Link>
-            <Link href="/formats" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.formats}</Link>
-            <Link href="/theory" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.theory}</Link>
-            <Link href="/discussion" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.discussion}</Link>
-            <Link href="/references" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.references}</Link>
-            <ToggleLang lang={lang} />
+        <nav className="navbar absolute px-4 py-2 2xl:px-10 2xl:py-5 w-full backdrop-blur bg-base/70 z-50 shadow-lg">
+            <div className="md:flex md:justify-between w-full hidden">
+                <Link href="/" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.introduction}</Link>
+                <Link href="/formats" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.formats}</Link>
+                <Link href="/theory" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.theory}</Link>
+                <Link href="/discussion" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.discussion}</Link>
+                <Link href="/references" className="btn btn-ghost 2xl:text-2xl">{NavbarDictionary[lang]?.references}</Link>
+                <ToggleLang lang={lang} />
+            </div>
+            <div className="md:hidden">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-2">
+                        <li>
+                            <Link href="/" className="2xl:text-2xl uppercase font-bold">{NavbarDictionary[lang]?.introduction}</Link>
+                        </li>
+                        <li>
+                            <Link href="/formats" className="2xl:text-2xl uppercase font-bold">{NavbarDictionary[lang]?.formats}</Link>
+                        </li>
+                        <li>
+                            <Link href="/theory" className="2xl:text-2xl uppercase font-bold">{NavbarDictionary[lang]?.theory}</Link>
+                        </li>
+                        <li>
+                            <Link href="/discussion" className="2xl:text-2xl uppercase font-bold">{NavbarDictionary[lang]?.discussion}</Link>
+                        </li>
+                        <li>
+                            <Link href="/references" className="2xl:text-2xl uppercase font-bold">{NavbarDictionary[lang]?.references}</Link>
+                        </li>
+                        <li className="">
+                        <ToggleLang lang={lang} />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
