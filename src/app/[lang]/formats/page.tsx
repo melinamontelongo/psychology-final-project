fe6b83a404685b60aa0cde91a8ee9b44a5e42821
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-09-04 22:00:10 -0300

    style: formats page paddings

--- src/app/[lang]/formats/page.tsx
+++ src/app/[lang]/formats/page.tsx
@@ -12,20 +12,22 @@ export default function FormatsPage({ params }: { params: { lang: string } }) {
                     return (
                         <div>
                             <h2 id="" className="mb-5 text-5xl font-bold 2xl:text-8xl cursor-pointer hover:scale-125 hover:text-primary transition-all">{format.title}</h2>
-                            {format.sections.map((section) => {
-                                return (
-                                    <div>
-                                        <h4 className="text-2xl 2xl:text-5xl font-bold">{section.title}</h4>
-                                        <p>{section.info}</p>
-                                    </div>
-                                )
-                            })}
-                            <div className="flex flex-row flex-wrap gap-2">
-                                {format.keywords.map((keyword) => {
+                            <div className="flex flex-col gap-5 mb-5">
+                                {format.sections.map((section) => {
                                     return (
-                                        <span key={keyword} className="badge badge-lg badge-primary">{keyword}</span>
+                                        <div key={section.title}>
+                                            <h4 className="text-2xl 2xl:text-5xl font-bold">{section.title}</h4>
+                                            <p>{section.info}</p>
+                                        </div>
                                     )
                                 })}
+                                <div className="flex flex-row flex-wrap gap-2">
+                                    {format.keywords.map((keyword) => {
+                                        return (
+                                            <span key={keyword} className="badge badge-lg badge-primary">{keyword}</span>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
                     )
