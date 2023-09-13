4cb14e7d35b5ee736eedca3c4e605e851811add5
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-09-13 16:38:04 -0300

    feat: update discussion content

--- src/app/[lang]/discussion/page.tsx
+++ src/app/[lang]/discussion/page.tsx
@@ -7,17 +7,20 @@ export default function DiscussionPage({ params }: { params: { lang: string } })
     const dictionary = DiscussionDictionary[params.lang];
     return (
         <div className="min-h-screen pt-20 flex flex-col justify-center items-center">
-            <div className="max-w-2xl 2xl:max-w-5xl">
+            <div className="max-w-screen-lg 2xl:max-w-screen-2xl mx-auto px-4">
                 {dictionary.discussion.map((discussion, i) => {
                     return (
                         <div key={`discussion-${i}`}>
-                            <h2 id="" className="mb-5 text-5xl font-bold 2xl:text-8xl cursor-pointer hover:scale-125 hover:text-primary transition-all">{discussion.title}</h2>
+                            <h2 id="" className="mb-5 text-5xl font-bold 2xl:text-8xl text-primary">{discussion.title}</h2>
                             <div className="flex flex-col gap-5 mb-5">
                                 {discussion.sections.map((section) => {
                                     return (
                                         <div key={section.title}>
-                                            <h4 className="text-2xl 2xl:text-5xl font-bold">{section.title}</h4>
-                                            <p>{section.info}</p>
+                                            <h4 className="text-2xl 2xl:text-5xl font-bold underline decoration-primary underline-offset-4">{section.title}</h4>
+                                            {section.info.map((info) => {
+                                                return <p key={info} className="text-xl 2xl:text-2xl">{info}</p>
+                                            })}
+
                                         </div>
                                     )
                                 })}
--- src/content/discussion.ts
+++ src/content/discussion.ts
@@ -3,7 +3,7 @@ interface DiscussionDictionaryEntry {
         title: string,
         sections: {
             title: string,
-            info: string,
+            info: string[],
         }[],
         keywords: string[],
     }[],
@@ -17,23 +17,49 @@ export const DiscussionDictionary: Record<string, DiscussionDictionaryEntry> = {
                 sections: [
                     {
                         title: "Relacionalidad",
-                        info: "Característica relacional de las tecnologías de grabación y reproducción, surgiendo así prácticas en conjunto a cierto soporte.",
+                        info: [
+                            "Característica relacional de las tecnologías de grabación y reproducción.",
+                            "Las prácticas surgen en conjunto a cierto soporte.",
+                            "Las tecnologías no crean prácticas, ni éstas crean tecnologías.",
+                            "Todo surge en relación.",
+                        ]
                     },
                     {
-                        title: "Opacidad",
-                        info: "La mayor problemática reside en la opacidad sin precedentes de la era del streaming.",
+                        title: "Caja negra",
+                        info: [
+                            "La mayor problemática reside en la opacidad sin precedentes de la era del streaming.",
+                            "La forma en que los algoritmos operan con los datos permanece oscurecida.",
+                            "Abrir la caja negra es develar lo social, ético y político que acompaña a estos procesos.",
+                        ]
                     },
                     {
                         title: "Nodos en una red",
-                        info: "Se puede visualizar una red de la que quien escucha o produce una canción es un nodo más, junto a datos y algoritmos, teniendo estos últimos también agencia.",
+                        info: [
+                            "Quien escucha o produce una canción es un nodo más de una extensa red.",
+                            "Los datos y algoritmos también conforman la red y tienen agencia.",
+                        ]
                     },
                     {
-                        title: "La digitalidad",
-                        info: "Los objetos de la digitalidad son inherentemente fluidos y relacionales.",
+                        title: "Los objetos de la digitalidad",
+                        info: [
+                            "Son inherentemente fluidos y relacionales.",
+                            "Su existencia e identidad se da en relación con su medio.",
+                        ]
                     },
                     {
-                        title: "La necesaria multiplicidad",
-                        info: "Es imprescindible el acercamiento desde múltiples vertientes que den cuenta de la complejidad de la problemática, contemplando lo socio-histórico y económico.",
+                        title: "Multiplicidad",
+                        info: [
+                            "Es imprescindible el acercamiento desde múltiples vertientes que den cuenta de la complejidad de la problemática.",
+                            "Se debe contemplar lo socio-histórico y económico.",
+                        ]
+                    },
+                    {
+                        title: "El horizonte",
+                        info: [
+                            "La escucha y producción musical a la luz de nuevos avances en las tecnologías digitales.",
+                            "El panorama de los NFT.",
+                            "Las implicaciones de la Inteligencia Artificial."
+                        ]
                     },
                 ],
                 keywords: [
@@ -48,23 +74,49 @@ export const DiscussionDictionary: Record<string, DiscussionDictionaryEntry> = {
                 sections: [
                     {
                         title: "Relational nature",
-                        info: "Recording and reproduction technologies are relational so the multiple practices emerge in conjunction with a certain format.",
+                        info: [
+                            "Relational nature of recording and reproduction technologies.",
+                            "Practices emerge in conjunction with a certain format.",
+                            "Technologies do not create practices, nor do practices create technologies.",
+                            "Everything arises in relation.",
+                        ]
                     },
                     {
-                        title: "Opacity",
-                        info: "The biggest problem lies in the unprecedented opacity of the streaming era.",
+                        title: "Black box",
+                        info: [
+                            "The biggest problem lies in the unprecedented opacity of the streaming era.",
+                            "How algorithms operate on data remains obscured.",
+                            "To open the black box is to unveil the social, ethical and political issues that accompany these processes.",
+                        ]
                     },
                     {
                         title: "Nodes in a network",
-                        info: "It is possible to visualize a network of which the listener or producer of a song is just another node, together with data and algorithms, the latter also having agency.",
+                        info: [
+                            "The listener or producer of a song is just another node in a vast network.",
+                            "Data and algorithms also make up the network and have agency.",
+                        ]
                     },
                     {
                         title: "The Digital",
-                        info: "Digital objects are inherently fluid and relational.",
+                        info: [
+                            "Digital objects are inherently fluid and relational.",
+                            "Their existence and identity is given in relation to its environment.",
+                        ]
                     },
                     {
-                        title: "The necessary multiplicity",
-                        info: "It is essential to approach the issue from multiple perspectives that take into account the complexity of the problem, contemplating the socio-historical and economic aspects.",
+                        title: "Multiplicity",
+                        info: [
+                            "It is essential to approach the issue from multiple perspectives that take into account the complexity of the problem.",
+                            "Socio-historical and economic aspects must be considered."
+                        ]
+                    },
+                    {
+                        title: "The horizon",
+                        info: [
+                            "Music listening and production in light of new advances in digital technologies.",
+                            "The NFT Landscape.",
+                            "The implications of Artificial Intelligence."
+                        ]
                     },
                 ],
                 keywords: [
